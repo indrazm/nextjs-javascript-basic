d541b7190f618d8e080f7759e821cf5cb90cbf2b
Indra Zulfi Mushoddaq <me@indrazm.com>
2024-01-19 10:00:05 +0700

    chore: initial setup

--- src/lib/generateId.ts
+++ src/lib/generateId.ts
@@ -1,4 +1,4 @@
-import { customAlphabet } from "nanoid";
-const nanoid = customAlphabet("1234567890abcdefghizklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 18);
+import { customAlphabet } from 'nanoid'
+const nanoid = customAlphabet('1234567890abcdefghizklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 18)
 
-export const generateId = (): string => `${process.env.IDENTIFIER_PREFIX}_${nanoid()}`;
+export const generateId = (): string => `${process.env.IDENTIFIER_PREFIX}_${nanoid()}`
